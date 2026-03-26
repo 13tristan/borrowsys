d0641d625bd618096171642676495438a26e76cd
tristan <orpillatristan10@gmail.com>
2026-03-26 22:08:39 +0800

    add connector j driver

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class Main {
    public static void main(String[] args) {
-      String url = "jdbc:mysql://127.0.0.1:3306/movies"; // JDBC URL;
+      String url = ""; // JDBC URL;
       String user = "root"; // USERNAME
       String password = ""; // PASSWORD
 
